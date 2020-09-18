057363ec56a882b4063eca33da766210ede93a26
Baran Kuzu <barannkuzu@gmail.com>
2020-09-18 19:47:06 +0300

    'animals-2.json' and 'animals-3.json' files are added and 'main.js' and 'index.html' files are updated.

--- jsLAb/ajaxjs/js/main.js
+++ jsLAb/ajaxjs/js/main.js
@@ -1,3 +1,43 @@
+// url adres for requesting 
+let url = 'https://raw.githubusercontent.com/eheperson/codingLAb/master/jsLAb/ajaxjs/json/animals-1.json'
+
+// select empty div in html page
+let animalContainer = document.getElementById("animal-info")
+
+// select specific button element from html page
+let btn = document.getElementById("btn");
+
+// Event Listener for button
+btn.addEventListener("click", function(){
+    // r : our request
+    let r = new XMLHttpRequest();
+
+    // GET  : receive, download data
+    // POST : send, upload data 
+    r.open('GET', url);
+
+    r.onload = function(){
+        // d : data
+        // JSON.parse() : convert text data to json format
+        let d = JSON.parse(r.responseText)
+        renderHtml(d);
+    };
+
+    r.send();
+});
+
+function renderHtml(data){
+    let htmlString = "";
+    for(i = 0; i<data.length; i++){
+        htmlString +=  "<p>" + data[i].name + "is a " + data[i].species +  ".</p>";
+    }  
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+};
+
+
+
+
+/*
 var myCat = {
     "name": "hosaf",
     "species" : "cat",
@@ -28,3 +68,4 @@ var thePets = [
 ]
 
 console.log("Name : ", thePets[1].name, " -- ", "Favorite Food : ", thePets[1].favFood)
+*/
