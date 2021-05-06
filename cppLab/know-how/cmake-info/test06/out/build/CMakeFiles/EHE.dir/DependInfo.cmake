56c64e6dcca1671cb0057f1c7aefdab046559afd
eheperson <barannkuzu@gmail.com>
2021-05-06 13:51:36 +0300

    cmake info files added

---
+++ cppLab/know-how/cmake-info/test06/out/build/CMakeFiles/EHE.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kuzub/Documents/workSpace/codes/cpp/tests/cmakeTest/test06/main.cpp" "/home/kuzub/Documents/workSpace/codes/cpp/tests/cmakeTest/test06/out/build/CMakeFiles/EHE.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../../external/glfw-master/include"
+  "../../Adder"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kuzub/Documents/workSpace/codes/cpp/tests/cmakeTest/test06/out/build/Adder/CMakeFiles/adder.dir/DependInfo.cmake"
+  "/home/kuzub/Documents/workSpace/codes/cpp/tests/cmakeTest/test06/out/build/external/glfw-master/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
